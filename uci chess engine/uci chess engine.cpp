0b3cd06f0dbff15f26c19e5051132337540c3741
flipwonderland <flipwonderland@gmail.com>
2025-01-20 11:29:27 -0500

    changed credit and included the code on the website instead of my weird version

--- uci chess engine/uci chess engine.cpp
+++ uci chess engine/uci chess engine.cpp
@@ -98,21 +98,22 @@ public:
 
 
 //got these from bluefever softwares series
+//it is also here https://www.chessprogramming.org/Looking_for_Magics
 const int bitTable[64] = {
 	63, 30, 3, 32, 25, 41, 22, 33, 15, 50, 42, 13, 11, 53, 19, 34, 61, 29, 2, 51, 21, 43, 45, 10, 18, 47, 1, 54, 9, 57, 0, 35,
 	62, 31, 40, 4, 49, 5, 52, 26, 60, 6, 23, 44, 46, 27, 56, 16, 7, 39, 48, 24, 59, 14, 12, 55, 38, 28, 58, 20, 37, 17, 36, 8
 };
 
-int popBit(u64* bb) {
+int popFirstBit(u64* bb) {
 	u64 b = *bb ^ (*bb - 1);
 	u32 fold = (unsigned)((b & 0xffffffff) ^ (b >> 32));
 	*bb &= (*bb - 1);
 	return bitTable[(fold * 0x783a9b23) >> 26]; //I have no idea what the point of this is
 }
 
-int countBits(u64 b) {
+int count_1s(u64 b) {
 	int r;
-	for (r = b; r++; b &= b - 1);
+	for (r = 0; b; r++, b &= b - 1);
 	return r;
 }
 
